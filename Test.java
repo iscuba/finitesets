7f9b86d452acd0cbd0d8006cd1e067295b0ded0d
Isabella Cuba <Isabella@Isabellas-MacBook-Pro.local>
2014-09-18 13:17:49 -0400

    Make a random finite set
    for testing

--- Test.java
+++ Test.java
@@ -5,17 +5,30 @@
  */
 
 package finitesets;
-
+import java.util.*;
 /**
  *
  * @author Isabella
  */
 public class Test {
     
-    public int ranNum(int min, int max){
-        double rand = new Math.andom();
-        return Math.random();
+    public int randNum(int min, int max){
+        Random rand = new Random();
+        int randNum = rand.nextInt((max-min) + 1) + min;
+        return randNum;
     }
     
-    public static 
+    public FiniteSets randSet(FiniteSets start, int length){
+        if (start.cardinality()==length){
+            return start;
+        } else {
+            int num = randNum(0 ,100);
+            if (start.member(num))
+                return randSet(start, length);
+            else 
+                return randSet(start.add(num), length);
+        }
+    }
+    
+    
 }
